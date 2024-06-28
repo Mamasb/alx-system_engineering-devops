93d6b6db5db9aecaa6101b16f6cc2ffdf531ffd5
mamasb <pkpipimwangi@gmail.com>
2024-06-29 01:42:53 +0300

    Using Puppet, install flask from pip3.

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,9 @@
+# install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
+}# install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
+}
